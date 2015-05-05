cbd40cd71b352685458fec23ab3c975cd2dd0b61
Christian Suter <christian.s@biffbangpow.com>
2015-05-05 12:12:18 +0100

    Updated DependencyInjection

--- DependencyInjection/Factory/Payment/SagepayOnsitePaymentFactory.php
+++ DependencyInjection/Factory/Payment/SagepayOnsitePaymentFactory.php
@@ -1,15 +1,8 @@
 <?php
 namespace Ledjin\Bundle\SagepayBundle\DependencyInjection\Factory\Payment;
 
-use Payum\Core\Exception\RuntimeException;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\DependencyInjection\Definition;
-use Symfony\Component\DependencyInjection\DefinitionDecorator;
-use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
-use Symfony\Component\Config\FileLocator;
 use Payum\Bundle\PayumBundle\DependencyInjection\Factory\Payment\AbstractPaymentFactory;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 
 class SagepayOnsitePaymentFactory extends AbstractPaymentFactory
 {
@@ -31,26 +24,22 @@ class SagepayOnsitePaymentFactory extends AbstractPaymentFactory
         $builder->children()
             ->scalarNode('vendor')->isRequired()->cannotBeEmpty()->end()
             ->booleanNode('sandbox')->defaultTrue()->end()
-            ->end();
+        ->end();
     }
 
     /**
      * {@inheritDoc}
      */
-    protected function addApis(Definition $paymentDefinition, ContainerBuilder $container, $contextName, array $config)
+    protected function getPayumPaymentFactoryClass()
     {
-        $apiDefinition = new DefinitionDecorator('payum.sagepay.api.prototype');
-        $apiDefinition->replaceArgument(
-            1,
-            array(
-                'vendor' => $config['vendor'],
-                'sandbox' => $config['sandbox'],
-            )
-        );
+        return 'Ledjin\Sagepay\OnsitePaymentFactory';
+    }
 
-        $apiDefinition->setPublic(true);
-        $apiId = 'payum.context.'.$contextName.'.api';
-        $container->setDefinition($apiId, $apiDefinition);
-        $paymentDefinition->addMethodCall('addApi', array(new Reference($apiId)));
+    /**
+     * {@inheritDoc}
+     */
+    protected function getComposerPackage()
+    {
+        return 'ledjin/sagepay';
     }
 }
